bf56f56957045ec084651e28d53394ad552ce850
Timur Jalilov <timurjalilov1@gmail.com>
2025-07-13 18:28:44 +0100

    feature: add pizza of the day section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Order from './Order'
+import PizzaOfTheDay from './PizzaOfTheDay'
 
 function App() {
   return (
     <div>
+      <h1 className="logo">Padre Gino's</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   )
 }
